27368221914b2998234200e49539f0cc05b5158e
DDD <dixond@optonline.net>
2013-10-26 21:30:01 -0400

    Initial Commit

---
+++ Out of this World/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  Out of this World
+//
+//  Created by DDD on 2013-10-26.
+//  Copyright (c) 2013 DDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/DDViewController.h
@@ -0,0 +1,13 @@
+//
+//  DDViewController.h
+//  Out of this World
+//
+//  Created by DDD on 2013-10-26.
+//  Copyright (c) 2013 DDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDViewController : UIViewController
+
+@end
